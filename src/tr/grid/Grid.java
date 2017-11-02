cb5a83dda64dc0894856bc9a9d775119a9ed8383
volt <voltdweller@gmail.com>
2017-11-01 21:13:24 -0400

    grid changes and oop is hard

--- src/tr/grid/Grid.java
+++ src/tr/grid/Grid.java
@@ -5,10 +5,47 @@
  */
 package tr.grid;
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  *
  * @author volt
  */
 public class Grid {
+    
+    String fileName = "grid.txt";
+    String line = null;
+    
+    public void FileOpen(){
+        
+        try{
+            
+            FileReader fileReader = new FileReader(fileName);
+            
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            
+            while((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+            
+        } catch(FileNotFoundException ex){
+            System.out.println("Unable to open file: '" + fileName + "'");
+        } catch(IOException ex){
+            System.out.println("Error reading file: '" + fileName + "'");
+        }
+        
+    }    
+    
+    // everything is initialized and created here
+    public static void main(String[] args){
+        
+        Grid gridder = new Grid();
+        
+        gridder.FileOpen();
+        
+    }
     
 }
